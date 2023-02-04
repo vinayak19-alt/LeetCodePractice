3551e441e0df8004ec27c01814eec1b913c6cea8
vinayak19-alt <80938731+vinayak19-alt@users.noreply.github.com>
2023-02-04 23:12:22 +0530

    Create Maximum product of three numbers.java

---
+++ Maximum product of three numbers.java
@@ -0,0 +1,10 @@
+Question link - https://leetcode.com/problems/maximum-product-of-three-numbers/
+
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n= nums.length;
+        int product=Math.max(nums[0]*nums[1]*nums[n-1], nums[n-1]*nums[n-2]*nums[n-3]);
+        return product;
+    }
+}
