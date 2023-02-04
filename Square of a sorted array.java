cba636065029910caf1d14936612aaecc2d6151a
vinayak19-alt <80938731+vinayak19-alt@users.noreply.github.com>
2023-02-04 23:17:33 +0530

    Create Square of a sorted array.java

---
+++ Square of a sorted array.java
@@ -0,0 +1,11 @@
+Question link - https://leetcode.com/problems/squares-of-a-sorted-array/description/
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            nums[i] = nums[i]*nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
