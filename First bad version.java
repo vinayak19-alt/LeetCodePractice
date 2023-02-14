4c27276e972db5d2c2f72447c225573ca6a10820
vinayak19-alt <80938731+vinayak19-alt@users.noreply.github.com>
2023-02-14 22:33:16 +0530

    Create First bad version.java

---
+++ First bad version.java
@@ -0,0 +1,21 @@
+Question link - https://leetcode.com/problems/first-bad-version/description/
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int low=0;
+        int high=n;
+        while(low<=high){
+            int mid= low + (high-low)/2;
+            if(isBadVersion(mid) == true && isBadVersion(mid-1) == false){
+                return mid;
+            }
+            else if(isBadVersion(mid) == false){
+                low= mid + 1;
+            }
+            else{
+                high=mid;
+            }
+        }
+        return 0;
+    }
+}
