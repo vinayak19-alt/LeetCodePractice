4a497e0af039b3ae7ea319b4c97657fd6347182b
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2023-02-26 22:12:28 +0530

    Create Number of good pairs.java

---
+++ Number of good pairs.java
@@ -0,0 +1,15 @@
+Question link - https://leetcode.com/problems/number-of-good-pairs/description/
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int count=0;
+        for(int i=0; i<nums.length; i++){
+            for(int j=0; j<nums.length; j++){
+                if(i<j && nums[i]==nums[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
