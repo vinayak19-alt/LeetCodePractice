dad23c529f0872879b652c3f207e99da96ffc98b
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2023-03-11 22:32:25 +0530

    Create Minimum number of moves to seat everyone.java

---
+++ Minimum number of moves to seat everyone.java
@@ -0,0 +1,13 @@
+Question link - https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/description/
+
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int noMoves = 0;
+        for (int i = 0; i < seats.length; ++i) {
+            noMoves += Math.abs(seats[i] - students[i]);
+        }
+        return noMoves;
+    }   
+}
