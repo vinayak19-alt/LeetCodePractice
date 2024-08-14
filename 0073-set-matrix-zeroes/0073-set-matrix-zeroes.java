dd879615d78d25d4d90dba8c21211a6c9e6fbc32
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2024-08-14 17:28:17 +0530

    Time: 1 ms (80.2%), Space: 45.7 MB (15.77%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,40 +1,21 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int col0 = 1;
-        int n = matrix.length;
-        int m = matrix[0].length;
+        int[] row = new int[matrix.length];
+        int[] col = new int[matrix[0].length];
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
                 if(matrix[i][j] == 0){
-                    //mark row to 0
-                    matrix[i][0]=0;
-                    //mark col to 0
-                    if(j!=0){
-                        matrix[0][j]=0;
-                    }else{
-                        col0=0;
-                    }
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
-        for(int i=1; i<n; i++){
-            for(int j=1; j<m; j++){
-                if(matrix[i][j]!=0){
-                    if(matrix[i][0] == 0 || matrix[0][j] == 0){
-                        matrix[i][j]=0;
-                    }
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(row[i]==1 || col[j]==1){
+                    matrix[i][j]=0;
                 }
-            }
-        }
-        if(matrix[0][0]==0){
-            for(int j=0; j<m; j++){
-                matrix[0][j]=0;
-            }
-        }
-        if(col0 == 0){
-            for(int i=0; i<n; i++){
-                matrix[i][0] =0;
             }
         }
     }
