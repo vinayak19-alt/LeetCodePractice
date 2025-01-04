22f27e8bcfbd380bf6bff7d4ee013353ca41fa04
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2025-01-04 09:30:06 +0530

    Time: 5 ms (27.31%), Space: 42.6 MB (12.99%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,26 +1,26 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
         if(digits.length()==0){
-            List<String> list = new ArrayList<String>();
+            List<String> list = new ArrayList<>();
             return list;
         }
         List<String> ans = new ArrayList<>();
         ans.addAll(helper("",digits));
         return ans;
     }
-    public List<String> helper(String p, String up){
+    private List<String> helper(String p, String up){
         if(up.isEmpty()){
             List<String> list = new ArrayList<>();
             list.add(p);
             return list;
         }
         int digit = up.charAt(0)-'0';
-        int i = (digit-2)*3;
+        int i= (digit-2)*3;
         if(digit>7){
             i+=1;
         }
         int len = i+3;
-        if(digit==7 || digit==9){
+        if(digit==7||digit==9){
             len+=1;
         }
         List<String> ans = new ArrayList<>();
