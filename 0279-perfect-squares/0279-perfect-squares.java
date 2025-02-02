c135fa1875c3726e552c3ee32b39d832699eeb9a
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2025-02-02 15:14:47 +0530

    Time: 25 ms (80.72%), Space: 42.9 MB (62.26%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -3,9 +3,9 @@ class Solution {
         int[] arr = new int[n+1];
         Arrays.fill(arr, Integer.MAX_VALUE);
         arr[0]=0;
-        int count=1;
-        while(count * count <= n){
-            int sq = count*count;
+        int count = 1;
+        while(count*count<=n){
+            int sq = count* count;
             for(int i=sq; i<=n; i++){
                 arr[i] = Math.min(arr[i-sq] + 1, arr[i]);
             }
