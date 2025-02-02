5af6e90a2fc6a403137f59e1c2f02fa7ce4cf53b
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2025-02-02 17:25:19 +0530

    Time: 4 ms (62.57%), Space: 48.3 MB (5.63%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -10,7 +10,7 @@ class Solution {
         }
         for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
-                grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+                grid[i][j] += Math.min(grid[i][j-1], grid[i-1][j]);
             }
         }
         return grid[m-1][n-1];
