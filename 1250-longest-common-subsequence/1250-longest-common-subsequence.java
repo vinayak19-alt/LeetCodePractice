cf0d8a2a16a0e9dcc1268f9d5e6560af0e019932
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2025-02-03 15:08:48 +0530

    Time: 20 ms (76.39%), Space: 51 MB (55.28%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -7,9 +7,9 @@ class Solution {
             for(int j=1; j<=n; j++){
                 if(text1.charAt(i-1) == text2.charAt(j-1)){
                     dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
                 }
-                dp[i][j] = Math.max(dp[i][j], dp[i-1][j]);
-                dp[i][j] = Math.max(dp[i][j], dp[i][j-1]);
             }
         }
         return dp[m][n];
