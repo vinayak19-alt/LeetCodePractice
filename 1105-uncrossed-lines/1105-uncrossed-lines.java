f034711abd448bf9cbc49c91b2f96944a50b5048
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2025-02-03 15:16:39 +0530

    Time: 8 ms (11.26%), Space: 44 MB (68.84%) - LeetHub

--- 1105-uncrossed-lines/1105-uncrossed-lines.java
+++ 1105-uncrossed-lines/1105-uncrossed-lines.java
@@ -7,9 +7,9 @@ class Solution {
             for(int j=1; j<=n; j++){
                 if(nums1[i-1] == nums2[j-1]){
                     dp[i][j] = dp[i-1][j-1]+1;
-                }else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
+                dp[i][j] = Math.max(dp[i][j], dp[i-1][j]);
+                dp[i][j] = Math.max(dp[i][j], dp[i][j-1]);
             }
         }
         return dp[m][n];
