0aa8cfc99b33a8bea804a4918ddce11469a26742
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2025-06-29 13:32:07 +0530

    Time: 2 ms (99.83%), Space: 47.3 MB (47%) - LeetHub

--- 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
@@ -1,20 +1,33 @@
 class Solution {
     public int[] sortArrayByParityII(int[] nums) {
-        int i=0, j=i+1;
-        while(j < nums.length){
-            if((nums[i]%2==0 && i%2!=0 && nums[j]%2!=0) || (nums[i]%2!=0 && i%2==0 && nums[j]%2==0)){
-                int num = nums[i];
-                nums[i] = nums[j];
-                nums[j] = num;
-                i++;
-                j=i+1;;
-            }else if((nums[i]%2==0 && i%2!=0 && nums[j]%2==0) || (nums[i]%2!=0 && i%2==0 && nums[j]%2!=0)){
-                j++;
-            }else{
-                i++;
-                j++;
+        // int i=0, j=i+1;
+        // while(j < nums.length){
+        //     if((nums[i]%2==0 && i%2!=0 && nums[j]%2!=0) || (nums[i]%2!=0 && i%2==0 && nums[j]%2==0)){
+        //         int num = nums[i];
+        //         nums[i] = nums[j];
+        //         nums[j] = num;
+        //         i++;
+        //         j=i+1;;
+        //     }else if((nums[i]%2==0 && i%2!=0 && nums[j]%2==0) || (nums[i]%2!=0 && i%2==0 && nums[j]%2!=0)){
+        //         j++;
+        //     }else{
+        //         i++;
+        //         j++;
+        //     }
+        // }
+        // return nums;
+        int e=0, o=1;
+        int[] arr = new int[nums.length];
+        for(int num : nums){
+            if(num % 2 ==0){
+                arr[e] = num;
+                e += 2;
+            }
+            if(num %2 !=0){
+                arr[o] = num;
+                o += 2;
             }
         }
-        return nums;
+        return arr;
     }
 }
