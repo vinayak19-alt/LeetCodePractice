671d3f60b9d6562948ccf4e0cd1249af9159fdc5
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2025-08-15 19:28:57 +0530

    Time: 2 ms (98.95%), Space: 45.1 MB (30.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0; i<nums.length; i++){
             int comp = target - nums[i];
             if(map.containsKey(comp)){
-                return new int[]{map.get(comp), i};
+                return new int[]{i, map.get(target-nums[i])};
             }
             map.put(nums[i], i);
         }
