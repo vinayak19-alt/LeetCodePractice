a9a0e48d1fbbf9fe2cbe762fe205e50689344e5b
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2025-08-27 22:15:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,15 +1,14 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i=0, j=0;
-        while(i < nums1.length && j < nums2.length){
-            if(nums1[i] > nums2[j]  && m < nums1.length){
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i] > nums2[j] && m < nums1.length){
                 shift(nums1, i);
-                int num = nums1[i];
                 nums1[i] = nums2[j];
+                m++;
                 i++;
                 j++;
-                m++;
-            }else if(i==m && nums1[m] == 0){
+            }else if(i == m || nums1[i] == 0){
                 nums1[m] = nums2[j];
                 m++;
                 j++;
@@ -18,10 +17,10 @@ class Solution {
             }
         }
     }
-    public void shift(int[] arr, int i){
-        for(int j=arr.length-1; j > i; j--){
-            arr[j] = arr[j-1];
+    private void shift(int[] nums1, int i){
+        for(int j = nums1.length-1; j>i; j--){
+            nums1[j] = nums1[j-1];
         }
-        arr[i]=0;
+        nums1[i] = 0;
     }
 }
