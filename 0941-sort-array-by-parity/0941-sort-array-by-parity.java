877a4d72e6287bc15f134a948231d90518bd11f0
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2025-09-01 22:03:19 +0530

    Time: 0 ms (100%), Space: 44.7 MB (96.4%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
         int i=0, j=nums.length-1;
-        while(i<j){
-            if(nums[i] % 2!=0 && nums[j]%2==0){
-                int num = nums[i];
-                nums[i] = nums[j];
-                nums[j]=num;
-            }else if(nums[i]%2!=0 && nums[j]%2!=0){
+        while(i < j){
+            if(nums[i] % 2 != 0 && nums[j] % 2 == 0){
+                swap(nums, i, j);
+                i++;
                 j--;
-            }else if(nums[i]%2==0 && nums[j]%2==0){
+            }else if(nums[i] % 2 != 0 && nums[j] % 2 != 0){
+                j--;
+            }else if(nums[i] % 2 == 0 && nums[j] % 2 == 0){
                 i++;
             }else{
                 i++;
@@ -16,5 +16,10 @@ class Solution {
             }
         }
         return nums;
+    }
+    public void swap(int[] arr , int i, int j){
+        int num = arr[i];
+        arr[i] = arr[j];
+        arr[j] = num;
     }
 }
