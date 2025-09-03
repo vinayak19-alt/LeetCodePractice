d8fc5771c6b2385a60978dd32f7450edd3f53cc4
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2025-09-03 22:03:10 +0530

    Time: 5 ms (76.25%), Space: 68.3 MB (23.08%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
+        int left=0, right=nums.length-1;
         int[] arr = new int[nums.length];
-        int left =0, right = nums.length-1;
-        for(int i=0, j=nums.length-1 ; i<nums.length;i++,j--){
+        for(int i=0, j=nums.length-1 ; i<nums.length ; i++, j--){
             if(nums[i] < pivot){
                 arr[left] = nums[i];
                 left++;
             }
-            if(nums[j] > pivot) {
+            if(nums[j] > pivot){
                 arr[right] = nums[j];
                 right--;
             }
