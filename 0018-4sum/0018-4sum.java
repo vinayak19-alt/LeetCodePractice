4ea68b0063d1fa6610a437869904a5fef7ab8def
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2025-09-16 23:00:26 +0530

    Time: 17 ms (72.74%), Space: 44 MB (62.71%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -5,32 +5,29 @@ class Solution {
             return list;
         }
         Arrays.sort(nums);
-        int n = nums.length;
-        for(int i=0; i<n-3; i++){
+        for(int i=0; i<nums.length-3; i++){
             if(i > 0 && nums[i] == nums[i-1]){
                 continue;
             }
-            for(int j= i+1; j<n-2; j++){
+            for(int j=i+1; j<nums.length-2; j++){
                 if(j>i+1 && nums[j] == nums[j-1]){
                     continue;
                 }
                 int k = j+1;
-                int l = n-1;
+                int l = nums.length-1;
                 while(k < l){
-                    long total = (long) nums[i] + nums[j] + nums[k] + nums[l];
-                    if(total < target){
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                    if(sum < target){
                         k++;
-                    }
-                    else if(total > target){
+                    }else if(sum > target){
                         l--;
-                    }else{
+                    }else if(sum == target){
                         list.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
                         k++;
-                        l--;
                         while(k < l && nums[k] == nums[k-1]){
                             k++;
                         }
-                        while(k < l && nums[l] == nums[l+1]){
+                        while(k < l && nums[l] == nums[l-1]){
                             l--;
                         }
                     }
