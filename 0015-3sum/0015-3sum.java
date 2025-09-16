8228a167909b370887dd7a674e2459c9a7cab1ce
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2025-09-16 22:21:23 +0530

    Time: 28 ms (81.19%), Space: 51.9 MB (35.89%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,25 +1,21 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if(nums.length == 0){
-            return ans;
-        }
+        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-        int n = nums.length;
-        for(int i=0; i< nums.length; i++){
-            if(i>0 && nums[i] == nums[i-1]){
+        for(int i=0; i<nums.length; i++){
+            if(i > 0 && nums[i] == nums[i-1]){
                 continue;
             }
             int j = i+1;
-            int k = n-1;
+            int k= nums.length-1;
             while(j < k){
-                int total = nums[i] + nums[j] + nums[k];
-                if(total < 0){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0){
                     j++;
-                }else if(total > 0){
+                }else if(sum > 0){
                     k--;
-                }else {
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                }else{
+                    list.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     j++;
                     while(j < k && nums[j] == nums[j-1]){
                         j++;
@@ -27,6 +23,6 @@ class Solution {
                 }
             }
         }
-        return ans;
+        return list;
     }
 }
