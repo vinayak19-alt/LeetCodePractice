471b028f178e9f26302c4cddaec4a2689c09eb76
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2025-09-23 22:16:08 +0530

    Time: 4 ms (86.19%), Space: 45.2 MB (88.55%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,5 +1,10 @@
 class Solution {
     public String reverseWords(String s) {
+        //we can do this question by adding a space after s and keep the code same and
+        //just return trim of the string to get rid of the last space you added
+        //but in java Strings are immutable so if you add anything to a string it will
+        //create a new memory address, if we want to avoid that we need to use line 25-33
+        //to just check if i is at lat of array the reverse the string
         char[] arr = s.toCharArray();
         int i=0;
         int j=i;
