4301b8e9ec05b9e723294a4e94c7a900b7f6f947
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2026-03-08 18:13:00 +0530

    Time: 2 ms (81.82%), Space: 46.4 MB (91.84%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,50 +1,27 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         char[][] board = new char[n][n];
-        for(int i=0; i<board.length; i++){
-            for(int j=0; j<board.length; j++){
-                board[i][j]='.';
+        List<List<String>> ans = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                board[i][j] = '.';
             }
         }
-        List<List<String>> list = new ArrayList<>();
-        queens(board, list, 0);
-        return list;
+        helper(board, ans, 0);
+        return ans;
     }
-    public void queens(char[][] board, List<List<String>> list, int row){
+    public void helper(char[][] board, List<List<String>> ans, int row){
         if(row == board.length){
-            list.add(addRow(board));
+            ans.add(addRow(board));
             return;
         }
-        for(int col=0; col<board.length; col++){
-            if(isSafe(board, row, col)){
-                board[row][col]='Q';
-                queens(board, list, row+1);
-                board[row][col]='.';
+        for(int col = 0; col < board.length; col++){
+            if(isSafe(row, col, board)){
+                board[row][col] = 'Q';
+                helper(board, ans, row+1);
+                board[row][col] = '.';
             }
         }
-    }
-    public boolean isSafe(char[][] board, int row, int col){
-        //check vertically
-        for(int i=0; i<row; i++){
-            if(board[i][col]=='Q'){
-                return false;
-            }
-        }
-        //check left diagonal
-        int maxLeft = Math.min(row, col);
-        for(int i=1; i<=maxLeft; i++){
-            if(board[row-i][col-i]=='Q'){
-                return false;
-            }
-        }
-        //check right diagonal
-        int maxRight = Math.min(row, board.length-col-1);
-        for(int i=1; i<=maxRight; i++){
-            if(board[row-i][col+i]=='Q'){
-                return false;
-            }
-        }
-        return true;
     }
     public List<String> addRow(char[][] board){
         List<String> list = new ArrayList<>();
@@ -53,5 +30,25 @@ class Solution {
             list.add(row);
         }
         return list;
+    }
+    public boolean isSafe(int row, int col, char[][] board){
+        for(int i=0; i<row; i++){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+        int maxLeft = Math.min(row, col);
+        for(int i=1; i<=maxLeft; i++){
+            if(board[row-i][col-i] == 'Q'){
+                return false;
+            }
+        }
+        int maxRight = Math.min(row, board.length-col-1);
+        for(int i=1; i<=maxRight; i++){
+            if(board[row-i][col+i] == 'Q'){
+                return false;
+            }
+        }
+        return true;
     }
 }
