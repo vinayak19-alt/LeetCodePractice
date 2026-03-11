63028dc20a975876f4bb4ef1d12b539a730473a6
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2026-03-11 22:18:16 +0530

    Time: 372 ms (39.87%), Space: 42.8 MB (67.87%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,69 +1,39 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        sudoku(board);
+        helper(board);
     }
-
-    public boolean sudoku(char[][] board) {
-        int n = board.length;
-        int row = -1, col = -1;
-        boolean emptyLeft = false;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == '.') {
-                    row = i;
-                    col = j;
-                    emptyLeft = true;
-                    break;
-                }
-            }
-            if (emptyLeft) {
-                break;
-            }
-        }
-
-        // If no empty cell is left, Sudoku is solved
-        if (!emptyLeft) {
-            return true;
-        }
-
-        // Try placing digits 1-9 in the current empty cell
-        for (int number = 1; number <= 9; number++) {
-            char num = (char) (number + '0');
-            if (isSafe(board, row, col, num)) {
-                board[row][col] = num;
-                if (sudoku(board)) {
-                    return true;
-                }
-                board[row][col] = '.';
-            }
-        }
-        return false;
-    }
-
-    public boolean isSafe(char[][] board, int row, int col, char num) {
-        for (int i = 0; i < board.length; i++) {
-            if (board[row][i] == num) {
-                return false;
-            }
-        }
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][col] == num) {
-                return false;
-            }
-        }
-
-        int sqrt = (int) Math.sqrt(board.length);
-        int rowStart = row - row % sqrt;
-        int colStart = col - col % sqrt;
-
-        for (int r = rowStart; r < rowStart + sqrt; r++) {
-            for (int c = colStart; c < colStart + sqrt; c++) {
-                if (board[r][c] == num) {
+    public boolean helper(char[][] board){
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                if(board[i][j] == '.'){
+                    for(char c='1'; c<='9'; c++){
+                        if(isSafe(board, i, j, c)){
+                            board[i][j] = c;
+                            if(helper(board) == true){
+                                return true;
+                            }else{
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
                     return false;
                 }
             }
         }
-
-        return true; 
+        return true;
+    }
+    public boolean isSafe(char[][] board, int row, int col, char c){
+        for(int i=0; i<9; i++){
+            if(board[row][i] == c){
+                return false;
+            }
+            if(board[i][col] == c){
+                return false;
+            }
+            if(board[3*(row/3) + i/3][3*(col/3) + i%3] == c){
+                return false;
+            }
+        }
+        return true;
     }
 }
