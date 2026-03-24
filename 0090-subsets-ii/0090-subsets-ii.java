f9aff68c5769be2ab26636fe182496eda02012fe
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2026-03-24 22:44:03 +0530

    Time: 2 ms (99.5%), Space: 45.2 MB (36.25%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,25 +1,17 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
-        helper(result, new ArrayList<>(), nums, 0);
-        return result;
+        List<List<Integer>> list = new ArrayList<>();
+        helper(nums, list, new ArrayList<>(), 0);
+        return list;
     }
-    public void helper(List<List<Integer>> result, List<Integer> list, int[] arr, int index){
-        result.add(new ArrayList<>(list));
-        for(int i = index; i<arr.length; i++){
-            /*
-            to put it simply we are doing this check to avoid duplicate subsets in
-            the result list, because if let's say after a recusrion call we are at index 2
-            then we would take it because we got ata new index but in that same same recursion
-            call if the next element is same then we would check that if we landed on a
-            new element or we are next to that element and it is same as the last do we
-            ignore it to keep our subsets unique
-            */
+    public void helper(int[] arr, List<List<Integer>> list, List<Integer> seq, int index){
+        list.add(new ArrayList<>(seq));
+        for(int i=index; i<arr.length; i++){
             if(i != index && arr[i] == arr[i-1]) continue;
-            list.add(arr[i]);
-            helper(result, list, arr, i+1);
-            list.remove(list.size()-1);
+            seq.add(arr[i]);
+            helper(arr, list, seq, i+1);
+            seq.remove(seq.size()-1);
         }
     }
 }
