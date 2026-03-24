94cc6956445c0d033df39bee7aab42d7b5bcb582
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2026-03-24 22:07:46 +0530

    Time: 2 ms (95.51%), Space: 45.9 MB (45.41%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,21 +1,21 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> list = new ArrayList<>();
-        helper(list, new ArrayList<>(), candidates, target, 0);
-        return list;
+        List<List<Integer>> result = new ArrayList<>();
+        helper(candidates, target, result, new ArrayList<>(), 0, 0);
+        return result;
     }
-    public void helper(List<List<Integer>> result, List<Integer> seq, int[] arr, int target, int index){
+    public void helper(int[] arr, int target, List<List<Integer>> list, List<Integer> seq, int sum, int index){
         if(index == arr.length){
-            if(target == 0){
-                result.add(new ArrayList<>(seq));
+            if(sum == target){
+                list.add(new ArrayList<>(seq));
             }
             return;
         }
-        if(arr[index] <= target){
+        if(target-sum >= arr[index]){
             seq.add(arr[index]);
-            helper(result, seq, arr, target-arr[index], index);
+            helper(arr, target, list, seq, sum+arr[index], index);
             seq.remove(seq.size()-1);
         }
-        helper(result, seq, arr, target, index+1);
+        helper(arr, target, list, seq, sum, index+1);
     }
 }
