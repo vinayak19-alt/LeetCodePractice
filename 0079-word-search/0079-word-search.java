aa45cb4e424082d568c37a31f64fa37b7b30d01a
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2026-03-28 16:48:52 +0530

    Time: 155 ms (31.08%), Space: 43 MB (69.8%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,28 +1,40 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
+        int[][] vis = new int[board.length][board[0].length];
+        int row = -1;
+        int col = -1;
+        int[] di = {1, 0, 0, -1};
+        int[] dj = {0, -1, 1, 0};
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[0].length; j++){
-                if(helper(board, word, i, j, 0)){
-                    return true;
+                if(board[i][j] == word.charAt(0)){
+                    vis[i][j] = 1;
+                    if(helper(board, word, i, j, di, dj, vis, 1)){
+                        return true;
+                    }
+                    vis[i][j] = 0;
                 }
             }
         }
         return false;
     }
-    public boolean helper(char[][] board, String word, int r, int c, int index){
-        if(index==word.length()){
+    public boolean helper(char[][] board, String word, int row, int col, int[] di, int[] dj, int[][] vis, int index){
+        if(index == word.length()){
             return true;
         }
-        if(r<0 || c<0 || r>board.length-1 || c>board[0].length-1 || board[r][c] != word.charAt(index)){
-            return false;
+        for(int i=0; i<4; i++){
+            int nextRow = row + di[i];
+            int nextCol = col + dj[i];
+            if(nextRow >= 0 && nextCol >= 0 && nextRow < board.length && nextCol < board[0].length
+                && vis[nextRow][nextCol] != 1 && board[nextRow][nextCol] == word.charAt(index)){
+                char ch = board[nextRow][nextCol];
+                vis[nextRow][nextCol] = 1;
+                if(helper(board, word, nextRow, nextCol, di, dj, vis, index+1)){
+                    return true;
+                }
+                vis[nextRow][nextCol] = 0;
+            }
         }
-        char temp = board[r][c];
-        board[r][c]=' ';
-        boolean found = helper(board, word, r+1, c, index+1)||
-                        helper(board, word, r, c+1, index+1)||
-                        helper(board, word, r-1, c, index+1)||
-                        helper(board, word, r, c-1, index+1);
-        board[r][c]=temp;
-        return found;
+        return false;
     }
 }
