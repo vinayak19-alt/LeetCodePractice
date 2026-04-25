b39700ddfaef0301ef11f5fbe5341289e97481e3
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2026-04-25 16:12:42 +0530

    Time: 1 ms (90.67%), Space: 47.3 MB (85.01%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,17 +3,24 @@ class Solution {
         int[] row = new int[matrix.length];
         int[] col = new int[matrix[0].length];
         for(int i=0; i<matrix.length; i++){
-            for(int j=0; j < matrix[0].length; j++){
+            for(int j=0; j<matrix[0].length; j++){
                 if(matrix[i][j] == 0){
-                    row[i]=1;
-                    col[j]=1;
+                    row[i] = -1;
+                    col[j] = -1;
                 }
             }
         }
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                if(row[i] == 1 || col[j] == 1){
-                    matrix[i][j]=0;
+        for(int i=0; i<row.length; i++){
+            if(row[i] == -1){
+                for(int j=0; j<matrix[0].length; j++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for(int i=0; i<col.length; i++){
+            if(col[i] == -1){
+                for(int j=0; j<matrix.length; j++){
+                    matrix[j][i] = 0;
                 }
             }
         }
