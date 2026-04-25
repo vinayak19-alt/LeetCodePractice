98e0159cd874a1c6e8dfb828edadf9714ddff924
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2026-04-25 15:06:03 +0530

    Time: 0 ms (100%), Space: 44.9 MB (16.91%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,23 +1,18 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        //we have to traverse from the end of the array and find a number that is lesser than the
-        //previous number, then we have to find the number that is bigger than that number but
-        //smaller than the ones we already traversed, then we swap those two numbers and reverse
-        //the array from that point to the end to get the exact next permutation
         int i = nums.length-2;
-        while(i >= 0 && nums[i] >= nums[i+1]) i--;
-
+        while(i>=0 && nums[i] >= nums[i+1]) i--;
         if(i>=0){
-            int j= nums.length-1;
+            int j=nums.length-1;
             while(j>=0 && nums[j] <= nums[i]) j--;
             swap(nums, i, j);
         }
         reverse(nums, i+1);
     }
     public void swap(int[] arr, int i, int j){
-        int num = arr[i];
+        int temp = arr[i];
         arr[i] = arr[j];
-        arr[j] = num;
+        arr[j] = temp;
     }
     public void reverse(int[] arr, int start){
         int end = arr.length-1;
