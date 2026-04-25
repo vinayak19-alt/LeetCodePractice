e103b9f1a290e89c122188d2b8dca10bdbe19ed2
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2026-04-25 16:15:13 +0530

    Time: 1 ms (90.67%), Space: 47.3 MB (91.95%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -10,17 +10,10 @@ class Solution {
                 }
             }
         }
-        for(int i=0; i<row.length; i++){
-            if(row[i] == -1){
-                for(int j=0; j<matrix[0].length; j++){
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        for(int i=0; i<col.length; i++){
-            if(col[i] == -1){
-                for(int j=0; j<matrix.length; j++){
-                    matrix[j][i] = 0;
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(row[i] == -1 || col[j] == -1){
+                    matrix[i][j]=0;
                 }
             }
         }
